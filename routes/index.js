f3dad9e16c13a50d7974fbef107641feb6d35be7
Keith Yong <terda12@gmail.com>
2015-04-05 17:44:43 -0400

    signin/signup/signout routing

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,87 @@
 var express = require('express');
 var config = require('../config');
 var router = express.Router();
+var Model = require('../model.js');
 
-/* GET home page. */
+// ------------------------------
+// Home page
+// ------------------------------
 router.get('/', function(req, res, next) {
   res.render('index', { title: config.websiteName, subpy: 'Front Page' });
 });
+
+// ------------------------------
+// signin routes
+// ------------------------------
+router.get('/signin', function(req, res, next) {
+    if (req.isAuthenticated()) {
+        res.redirect('/');
+    } else {
+        res.render('signin', { title: 'Sign In' });
+    }
+});
+
+router.post('/signin', function(req, res, next) {
+    passport.authenticate('local', {
+                         successRedirect: '/',
+                         failureRedirect: '/signin'
+    }, function(err, user, info) {
+        if (err) {
+            return res.render('signin', { title: 'Sign In', errorMessage: err.message });
+        }
+
+        if (!user) {
+            return res.render('signin', { title: 'Sign In', errorMessage: info.message });
+        }
+
+        return req.logIn(user, function(err) {
+            if (err) {
+                return res.render('signin', { title: 'Sign In', errorMessage: err.message });
+            } else {
+                return res.redirect('/');
+            }
+        });
+    })(req, res, next);
+});
+
+// ------------------------------
+// signup routes
+// ------------------------------
+router.get('/signup', function(req, res, next) {
+    if (req.isAuthenticated()) {
+        res.redirect('/');
+    } else {
+        res.render('signup', { title: 'Sign Up' });
+    }
+});
+
+router.post('/signup', function(req, res, next) {
+    // Here, req.body is { username, password }
+    var user = req.body;
+
+    // Before making the account, try and fetch a username to see if it already exists.
+    var usernamePromise = new Model.User({ username: user.username }).fetch();
+
+    Model.createNewUser(user.username, user.password, function (err, newUser) {
+        if (err) {
+            res.render('signup', { title: 'signup', errorMessage: 'username already exists' });
+        } else {
+            res.redirect(307, '/signin');
+        }
+    });
+});
+
+// ------------------------------
+// singout route
+// ------------------------------
+router.get('/signout', function(req, res, next) {
+    if (!req.isAuthenticated()) {
+        res.redirect('/', { errorMessage: 'You are not logged in' });
+    } else {
+        req.logout();
+        res.redirect('/signin');
+    }
+});
+
 
 module.exports = router;
