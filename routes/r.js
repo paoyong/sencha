8584c1f9d83395904d2e6f29d0ed01d281598bde
Keith Yong <terda12@gmail.com>
2015-04-06 20:32:35 -0400

    404 error page, and redirect to 404 if subpy does not exist

--- routes/r.js
+++ routes/r.js
@@ -6,10 +6,18 @@ var Model = require('../model');
 router.get('/:subpy', function(req, res, next) {
     var subpy = req.params.subpy;
 
-    res.render('index', { 
-        title: config.websiteName,
-        subpy: subpy,
-        user: req.user
+    Model.doesSubpyExist(subpy, function(doesExist) {
+        if (doesExist === true) {
+            res.render('index', { 
+                title: config.websiteName,
+                subpy: subpy,
+                user: req.user
+            });
+        } else {
+            res.render('404', {
+                errorMessage: 'Sorry, subpy does not exist.'
+            });
+        }
     });
 });
 
